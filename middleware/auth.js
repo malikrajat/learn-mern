d75bcdd0b37cbf428c812c4dfee64ef85029ac19
er.rajatmalik <er.rajatmalik@gmail.com>
2020-07-11 09:22:17 +0530

    add auth pricess

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const auth = (req, res, next) => {
+	try {
+		const token = req.header("x-auth-token");
+		if (!token)
+			return res.status(401).json({ msg: "Token is not attached." });
+		const validateToken = jwt.verify(token, process.env.JWT_KEY);
+		if (!validateToken)
+			return res.status(401).json({ msg: "Token is not valid." });
+
+		req.user = validateToken.id;
+		console.log(validateToken);
+		next();
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+module.exports = auth;
